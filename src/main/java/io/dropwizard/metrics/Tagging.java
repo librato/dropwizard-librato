62a51678cf71258b1f5be6f3821bfee5a1c4c533
Collin Van Dyck <collinvandyck@gmail.com>
2016-11-16 12:23:17 -0500

    Feature/dropwizard 0.8.x tagging (#10)

---
+++ src/main/java/io/dropwizard/metrics/Tagging.java
@@ -0,0 +1,15 @@
+package io.dropwizard.metrics;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class Tagging {
+    @JsonProperty("enabled")
+    public Boolean enabled = false;
+    @JsonProperty("static")
+    public Map<String, String> staticTags = Collections.emptyMap();
+    @JsonProperty("environment")
+    public Map<String, String> environmentTags = Collections.emptyMap();
+}
